06cda5bda44fbe929c063d3e6ce3422cea219bc5
noura <noura.tu19@gmail.com>
2024-09-17 13:58:16 +0300

    fix

---
+++ profile.js
@@ -0,0 +1,21 @@
+
+const userName = document.getElementById("user-name");
+const userEmail = document.getElementById("user-email");
+
+fetch("https://66e7e6b3b17821a9d9da6ff8.mockapi.io/login")
+    .then((response) => {
+        if (!response.ok) throw new Error("Network response was not ok.");
+        return response.json();
+    })
+    .then((data) => {
+        data.forEach((user) => {
+        if (user.name && user.email) {
+            userName.textContent = user.name;
+            userEmail.textContent = user.email;
+
+        }
+        });
+    })
+    .catch((error) => {
+        console.error("There was a problem with the fetch operation:", error);
+    });
